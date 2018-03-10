a094035bd0b022eb0fd3e2409dc8b096c04afc1f
Sjors <sjorsottjes@gmail.com>
2018-03-10 08:59:39 +0100

    manually check first and last wallet (because of blockchain.info error)

--- resources/assets/js/components/BitcoinPage.vue
+++ resources/assets/js/components/BitcoinPage.vue
@@ -32,9 +32,14 @@
 
         data: () => ({
             wallets: [],
+            isOnFirstPage: false,
+            isOnLastPage: false,
         }),
 
         mounted() {
+            this.isOnFirstPage = this.page === '1';
+            this.isOnLastPage  = this.page === '904625697166532776746648320380374280100293470930272690489102837043110636675';
+
             let keyPairs = this.generateKeyPairs(128);
 
             keyPairs.forEach(keyPair => {
@@ -56,8 +61,6 @@
                 // Calculate the first seed for this page.
                 let bigInt = bigi(this.page).subtract(bigi.ONE).multiply(bigi(''+amount));
 
-                let isOnLastPage = this.page === '904625697166532776746648320380374280100293470930272690489102837043110636675';
-
                 let keyPairs = [];
 
                 for (let i = 0; i < amount; i++) {
@@ -67,7 +70,7 @@
                         new bitcoin.ECPair(bigInt, null, {compressed: false})
                     );
 
-                    if (isOnLastPage && bigInt.toString() === '115792089237316195423570985008687907852837564279074904382605163141518161494336') {
+                    if (this.isOnLastPage && bigInt.toString() === '115792089237316195423570985008687907852837564279074904382605163141518161494336') {
                         break;
                     }
                 }
@@ -76,17 +79,44 @@
             },
 
             loadBalances: function () {
-                let addresses = this.wallets.map(w => w.publicKey).join('|');
+                let addresses;
+
+                // Checking the balance of the first or last wallet on blockchain.info returns an error.
+                if (this.isOnFirstPage) {
+                    addresses = this.wallets.slice(1).map(w => w.publicKey).join('|');
+
+                    let firstWallet = this.wallets.find(w => w.publicKey === '1EHNa6Q4Jz2uvNExL497mE43ikXhwF6kZm');
+
+                    firstWallet.balance = 0;
+                    firstWallet.transactionCount = '99+';
+                    firstWallet.totalReceived = 4.86537461;
+                    firstWallet.loaded = true;
+                } else if (this.isOnLastPage) {
+                    addresses = this.wallets.slice(0, -1).map(w => w.publicKey).join('|');
+
+                    let lastWallet = this.wallets.find(w => w.publicKey === '1JPbzbsAx1HyaDQoLMapWGoqf9pD5uha5m');
+
+                    lastWallet.balance = 0;
+                    lastWallet.transactionCount = 11;
+                    lastWallet.totalReceived = 2.32500983;
+                    lastWallet.loaded = true;
+                } else {
+                    addresses = this.wallets.map(w => w.publicKey).join('|');
+                }
 
                 // http://keys.pk/api/v1/mock-balance?active=
                 // https://blockchain.info/balance?cors=true&active=
-                axios.get('http://keys.pk/api/v1/mock-balance?active='+addresses).then(response => {
+                axios.get('https://blockchain.info/balance?cors=true&active='+addresses).then(response => {
                     this.wallets.forEach(wallet => {
                         this.sleepRandom(3000).then(() => {
                             let data = response.data[wallet.publicKey];
 
-                            wallet.balance          = data.final_balance /  100000000;
-                            wallet.transactionCount = data.n_tx;
+                            if (data === undefined) {
+                                return;
+                            }
+
+                            wallet.balance          = data.final_balance / 100000000;
+                            wallet.transactionCount = data.n_tx ? (data.n_tx > 99 ? '99+' : data.n_tx) : 0;
                             wallet.totalReceived    = data.total_received / 100000000;
                             wallet.loaded           = true;
                         });
