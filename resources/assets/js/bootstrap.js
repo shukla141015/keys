b6e90be3c2a1f540babaef9959d9952f1c2829da
Sjors <sjorsottjes@gmail.com>
2018-08-23 19:10:25 +0200

    dont install jquery and lowdash

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,14 +1,7 @@
 // IE11 needs a promise polyfill
 require('es6-promise').polyfill();
 
-window._ = require('lodash');
-
-try {
-    window.$ = window.jQuery = require('jquery');
-} catch (e) {}
-
 window.axios = require('axios');
-
 
 // window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //
