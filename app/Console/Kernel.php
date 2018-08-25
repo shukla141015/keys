b2bf0d57ce79474cab132fa81f0df1b80d27ccd7
Sjors <sjorsottjes@gmail.com>
2018-08-25 17:18:52 +0200

    create CoinStats model for each day

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Models\CoinStats;
+use App\Support\Enums\CoinType;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -9,6 +11,10 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule)
     {
+        foreach (CoinType::all() as $coin) {
+            $schedule->call([CoinStats::class, 'today'], [$coin])->twiceDaily();
+        }
+
         // $schedule->command('sitemap:generate')->dailyAt('2:00');
     }
 }
