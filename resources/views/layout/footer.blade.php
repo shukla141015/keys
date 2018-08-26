46e627df89874b2a0b90b62e93010a8e8366764e
Sjors <sjorsottjes@gmail.com>
2018-08-26 14:07:21 +0200

    add a footer

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="py-2 mt-4 bg-white border-t text-xs">
+    <div class="flex flex-col sm:flex-row justify-around text-center max-w-md mx-auto">
+        <span class="mb-4 sm:mb-0">
+            Inspired by directory.io
+        </span>
+
+        <span class="mb-4 sm:mb-0">
+            Created by <a class="text-black hover:underline" href="https://twitter.com/SjorsOttjes" rel="nofollow" target="_blank">Sjors Ottjes</a>
+        </span>
+
+        <span class="mb-4 sm:mb-0">
+            <a class="text-black hover:underline" href="mailto:s.f.ottjes@gmail.com?subject=Keys.lol contact">Contact</a>
+        </span>
+    </div>
+</footer>
