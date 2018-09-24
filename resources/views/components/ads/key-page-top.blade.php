5ec3d7560959477e667ec67a84d230ac18f640b1
Sjors <sjorsottjes@gmail.com>
2018-09-24 11:31:16 +0200

    limit height of ads

--- resources/views/components/ads/key-page-top.blade.php
+++ resources/views/components/ads/key-page-top.blade.php
@@ -1,15 +1,17 @@
-@if(App::environment('production'))
-    <ins class="adsbygoogle mb-4"
-         style="display:block; height: 100px;"
-         data-ad-client="ca-pub-8027891891391991"
-         data-ad-slot="6714715571"
-         data-ad-format="horizontal"
-         data-full-width-responsive="true"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
-@else
-    <div class="block mb-4" style="height: 100px; border:1px solid black;">
-        key-page-top
-    </div>
-@endif
+<div class="mb-4" style="height: 100px;">
+    @if(App::environment('production'))
+        <ins class="adsbygoogle"
+             style="display:block"
+             data-ad-client="ca-pub-8027891891391991"
+             data-ad-slot="6714715571"
+             data-ad-format="horizontal"
+             data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    @else
+        <div style="height: 100px; border:1px solid black;">
+            key-page-top
+        </div>
+    @endif
+</div>
