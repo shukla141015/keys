86d991a2e966af7733e9091d9efa1d7e0a1f9b5c
Sjors <sjorsottjes@gmail.com>
2018-11-01 18:12:09 +0100

    unblock russian

--- app/Http/Middleware/FloodBlocker.php
+++ app/Http/Middleware/FloodBlocker.php
@@ -8,11 +8,11 @@ class FloodBlocker
 {
     public function handle($request, Closure $next)
     {
-        $userAgent = $request->header('User-Agent');
-
-        if (strpos($userAgent, '68.0.3440.106') !== false) {
-            abort(400);
-        }
+//        $userAgent = $request->header('User-Agent');
+//
+//        if (strpos($userAgent, '68.0.3440.106') !== false) {
+//            abort(400);
+//        }
 
         return $next($request);
     }
